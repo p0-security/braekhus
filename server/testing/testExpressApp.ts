2257a538aca005abdb019f13cfec1f2f7ab32bba
Gergely Danyi <gergely.danyi@p0.dev>
2023-08-01 17:24:02 -0500

    Test GHA

---
+++ server/testing/testExpressApp.ts
@@ -0,0 +1,27 @@
+import { createLogger } from "../../log";
+import express, { Router } from "express";
+import audit from "pino-http";
+
+const logger = createLogger({ name: "testHttpServer" });
+
+export const testHttpServer = (port: number) => {
+  const router = Router();
+
+  router.get("/", (req, res) => {
+    res.send("root");
+  });
+
+  router.get("/happy/path", (req, res) => {
+    res.send("hello");
+  });
+
+  const app = express();
+  app.use(audit({ logger }));
+  app.use("/", router);
+
+  const httpServer = app.listen(port, () => {
+    logger.info(`Test app listening on port ${port}`);
+  });
+
+  return httpServer;
+};
