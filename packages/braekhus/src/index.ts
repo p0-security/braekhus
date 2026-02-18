035f67757de7ef88d4c2d8ae63666c9f8be966d9
Tristan Sweeney <tristan.sweeney@p0.dev>
2026-02-18 15:22:49 -0800

    Drop export of retryWithBackoff

--- packages/braekhus/src/index.ts
+++ packages/braekhus/src/index.ts
@@ -1,6 +1,5 @@
 export { runApp } from "./server/index.js";
 export type { AppContext, InitContext } from "./server/index.js";
-export { retryWithBackoff } from "./client/backoff.js";
 export type { RetryOptions } from "./client/backoff.js";
 export type {
   PublicKeyGetter,
