5573fea67788490322ca3cf4db3bb03ef63ec556
Tristan Sweeney <sweeney.tr@husky.neu.edu>
2026-02-18 19:07:11 -0500

    Define `exports` + `bin` for package (#61)
    Added an `exports` key + `index.ts` to intentionally only export the one
    function + few types we expect people to use.
    
    Also, defines our cli via`bin`, so we become an executable once the
    package is installed. `npm i -g @p0security/braekhus` will put
    `braekhus` on the path now.
    
    Also, move source into a `src` dir.

---
+++ packages/braekhus/src/index.ts
@@ -0,0 +1,8 @@
+export { runApp } from "./server/index.js";
+export type { AppContext, InitContext } from "./server/index.js";
+export type { RetryOptions } from "./client/backoff.js";
+export type {
+  PublicKeyGetter,
+  CallOptions,
+  ForwardedRequestOptions,
+} from "./types/index.js";
